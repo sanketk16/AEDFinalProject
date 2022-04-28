8323dad5f2e9df7ce2692ae7578651da010b1c15
Shreya Gosrani <30554781+shreyagosrani9@users.noreply.github.com>
2022-04-28 11:37:02 -0400

    Add files via upload

---
+++ src/Business/Role/GymManagerRole.java
@@ -0,0 +1,31 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Enterprise.GymEnterprise;
+import Business.Enterprise.SalesEnterprise;
+import Business.Network.Network;
+import Business.Organization.CustomerOrganization;
+import Business.Organization.GymManagerOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import UI.Gym.Manager.GymManagerWorkAreaJPanel;
+
+/**
+ *
+ * @author luliu
+ */
+public class GymManagerRole extends Role{
+
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Network network, EcoSystem business) {
+         return new GymManagerWorkAreaJPanel(userProcessContainer, account, (GymManagerOrganization)organization, 
+                 (GymEnterprise)enterprise,network, business);
+    }
+    
+}
