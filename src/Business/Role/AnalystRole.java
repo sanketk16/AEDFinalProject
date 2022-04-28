2fce2d3cfb4b3b973125402239da079cca76f637
sanketk16 <khadke.s@northeastern.edu>
2022-04-27 22:48:15 -0400

    Create AnalystRole.java

---
+++ src/Business/Role/AnalystRole.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Role;
+
+import Business.EcoSystem;
+import Business.Enterprise.Enterprise;
+import Business.Enterprise.GymEnterprise;
+import Business.Enterprise.SalesEnterprise;
+import Business.Network.Network;
+import Business.Organization.AnalysisOrganization;
+import Business.Organization.Organization;
+import Business.UserAccount.UserAccount;
+import javax.swing.JPanel;
+import UI.Gym.Analyst.AnalysisJPanel;
+
+/**
+ *
+ * @author sanket
+ */
+public class AnalystRole extends Role{
+    
+    @Override
+    public JPanel createWorkArea(JPanel userProcessContainer, UserAccount account, Organization organization, Enterprise enterprise, Network network, EcoSystem business) {
+        SalesEnterprise ols = null;
+//        for(Enterprise ent : network.getEnterpriseDirectory().getEnterpriseList()){
+//            if(ent instanceof SalesEnterprise){
+//                ols = (SalesEnterprise)ent;
+//                break;
+//            }
+//        }
+//        if(ols == null){
+//            return null;
+//        }
+        return new AnalysisJPanel(userProcessContainer, (GymEnterprise)enterprise, ols);
+    }
+    
+}
