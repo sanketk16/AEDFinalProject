81af3e6d748962555707c749bf80092bb44033b7
Shreya Gosrani <30554781+shreyagosrani9@users.noreply.github.com>
2022-04-29 14:08:52 -0400

    Add files via upload

--- src/UI/Customer/OrderItemJPanel.java
+++ src/UI/Customer/OrderItemJPanel.java
@@ -317,20 +317,7 @@ public class OrderItemJPanel extends javax.swing.JPanel {
         }
     }//GEN-LAST:event_addBtnActionPerformed
 
-    private void deleteBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_deleteBtnActionPerformed
-        // TODO add your handling code here:
-        int selectedRow = cartJTable.getSelectedRow();
-        if(selectedRow >= 0){
-            int selectionButton = JOptionPane.YES_NO_OPTION;
-            int selectionResult = JOptionPane.showConfirmDialog(null, "Are you sure to delete?","Warning",selectionButton);
-            if(selectionResult == JOptionPane.YES_OPTION){
-                Sales product = (Sales)cartJTable.getValueAt(selectedRow, 0);
-                cart.remove(product);
-                populateCart();
-            }
-        }else{
-            JOptionPane.showMessageDialog(null, "Please select a Row!!");
-        }
+    
     }//GEN-LAST:event_deleteBtnActionPerformed
 
     private void checkoutBtnActionPerformed(java.awt.event.ActionEvent evt) {//GEN-FIRST:event_checkoutBtnActionPerformed
@@ -381,7 +368,11 @@ public class OrderItemJPanel extends javax.swing.JPanel {
     private javax.swing.JButton checkoutBtn;
     private javax.swing.JButton deleteBtn;
     private javax.swing.JTable itemMenuJTable;
-    
+    private javax.swing.JLabel jLabel1;
+    private javax.swing.JLabel jLabel2;
+    private javax.swing.JLabel jLabel3;
+    private javax.swing.JLabel jLabel4;
+    private javax.swing.JPanel jPanel1;
     private javax.swing.JScrollPane jScrollPane1;
     private javax.swing.JScrollPane jScrollPane2;
     private javax.swing.JLabel priceLabel;
