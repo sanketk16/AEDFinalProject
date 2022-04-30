4f6538a7c347fad5be0e6ffce940119b95db66c3
sanketk16 <khadke.s@northeastern.edu>
2022-04-30 17:29:21 -0400

    Create ProductFleet.java

---
+++ src/Business/Product/ProductFleet.java
@@ -0,0 +1,28 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.Product;
+
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sanket
+ */
+public class ProductFleet {
+    private ArrayList<ProductFleet> productFleet;
+    
+    public ProductFleet(){
+        this.productFleet = productFleet;
+    }
+
+    public ArrayList<ProductFleet> getProductFleet() {
+        return productFleet;
+    }
+
+    public void setProductFleet(ArrayList<ProductFleet> productFleet) {
+        this.productFleet = productFleet;
+    }
+}
