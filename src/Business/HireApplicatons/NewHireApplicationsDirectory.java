e362e5c26be8eb97093743de69579fa984cd0aa7
sanketk16 <khadke.s@northeastern.edu>
2022-04-30 17:24:52 -0400

    Add files via upload

---
+++ src/Business/HireApplicatons/NewHireApplicationsDirectory.java
@@ -0,0 +1,30 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.HireApplications;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author sanket
+ */
+public class NewHireApplicationsDirectory {
+    
+    private ArrayList<NewHireApplication> newHireApplication;
+    
+    public NewHireApplicationsDirectory () {
+        this.newHireApplication = newHireApplication;
+    }
+
+    public ArrayList<NewHireApplication> getNewHireApplication() {
+        return newHireApplication;
+    }
+
+    public void setNewHireApplication(ArrayList<NewHireApplication> newHireApplication) {
+        this.newHireApplication = newHireApplication;
+    }
+    
+    
+}
