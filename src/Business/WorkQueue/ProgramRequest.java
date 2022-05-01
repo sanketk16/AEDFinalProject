e6f6aa3c665ac1ac4454ea09ea3e5814ff991dd5
Shreya Gosrani <30554781+shreyagosrani9@users.noreply.github.com>
2022-05-01 15:40:01 -0400

    Add files via upload
    v1

---
+++ src/Business/WorkQueue/ProgramRequest.java
@@ -0,0 +1,53 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.WorkQueue;
+import Business.Program.Program;
+import java.util.ArrayList;
+/**
+ *
+ * @author shreya
+ */
+public class ProgramRequest extends WorkRequest{
+    //private Program course;
+    private Program program;
+    private String result;
+    //private Program courserequest;
+    
+//    public ProgramRequest(Program courserequest){
+//    this.courserequest =  courserequest;
+//}
+
+    @Override
+    public String toString(){
+        
+        return program.getProgramName();
+    }
+    
+    public ProgramRequest(){
+        program = new Program();
+    }
+
+//    public Program getCourse() {
+//        return course;
+//    }
+//
+//    public void setCourse(Program course) {
+//        this.course = course;
+//    }
+
+    
+    
+//    public Program getCourserequest() {
+//        return courserequest;
+//    }
+//
+//    public void setCourserequest(Program courserequest) {
+//        this.courserequest = courserequest;
+//    }
+    
+    
+    
+}
