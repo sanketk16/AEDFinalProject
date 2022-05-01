c967a0d0bfb62c0cf8daa1fa21a33f317533f07a
Shreya Gosrani <30554781+shreyagosrani9@users.noreply.github.com>
2022-05-01 15:38:24 -0400

    Add files via upload

---
+++ src/Business/WorkQueue/ProductAvailabilityQueue.java
@@ -0,0 +1,21 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business.WorkQueue;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shreya
+ */
+public class ProductAvailabilityQueue {
+    private ArrayList<ProductAvailabilityQueue> productAvailabilityList;
+    
+    public ProductAvailabilityQueue(){
+        this.productAvailabilityList = productAvailabilityList;
+    }
+
+   
+}
