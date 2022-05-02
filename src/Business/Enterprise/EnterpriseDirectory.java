6d9c64eb514bc862691900573e2eaba9c5658603
Shreya Gosrani <30554781+shreyagosrani9@users.noreply.github.com>
2022-05-01 20:34:43 -0400

    Add files via upload

---
+++ src/Business/Enterprise/EnterpriseDirectory.java
@@ -0,0 +1,45 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Business.Enterprise;
+
+import Business.Organization.OrganizationDirectory;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author shreya
+ */
+public class EnterpriseDirectory {
+    private ArrayList<Enterprise> enterpriseList;
+   
+
+    public ArrayList<Enterprise> getEnterpriseList() {
+        return enterpriseList;
+    }
+
+    public void setEnterpriseList(ArrayList<Enterprise> enterpriseList) {
+        this.enterpriseList = enterpriseList;
+    }
+    
+    public EnterpriseDirectory(){
+        enterpriseList=new ArrayList<Enterprise>();
+    }
+    
+    //Create enterprise
+    public Enterprise createAndAddEnterprise(String name,Enterprise.EnterpriseType type){
+        Enterprise enterprise=null;
+        if(type==Enterprise.EnterpriseType.Gym){
+            enterprise=new GymEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        
+        if(type==Enterprise.EnterpriseType.Sales){
+            enterprise=new SalesEnterprise(name);
+            enterpriseList.add(enterprise);
+        }
+        return enterprise;
+    }
+}
